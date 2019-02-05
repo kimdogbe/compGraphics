feac5be11bdf3c11aa11fe86ba80a490c7905e43
kimdogbe <kimdogeb@gmail.com>
2019-02-05 17:57:39 +0000

    Implemented math for intersection

--- COMS30115/Labs/raytracer/Source/skeleton.cpp
+++ COMS30115/Labs/raytracer/Source/skeleton.cpp
@@ -11,10 +11,20 @@ using glm::mat3;
 using glm::vec4;
 using glm::mat4;
 
-
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 256
 #define FULLSCREEN_MODE false
+
+
+
+struct Intersection
+{
+vec4 position;
+float distance;
+int triangleIndex;
+};
+
+vector<Triangle> triangles;
 
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
@@ -24,9 +34,12 @@ void Draw(screen* screen);
 
 int main( int argc, char* argv[] )
 {
-  
+
+  /*fill vector of triangles*/
+  LoadTestModel(triangles );
+
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
-  
+
   while( NoQuitMessageSDL() )
     {
       Update();
@@ -45,7 +58,7 @@ void Draw(screen* screen)
 {
   /* Clear buffer */
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  
+
   vec3 colour(1.0,0.0,0.0);
   for(int i=0; i<1000; i++)
     {
@@ -66,4 +79,28 @@ void Update()
   /*Good idea to remove this*/
   std::cout << "Render time: " << dt << " ms." << std::endl;
   /* Update variables*/
+}
+
+// Start position of a ray, direction of the ray, triangles to check, return value if true
+bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles,
+   Intersection& closestIntersection ){
+
+  for(size_t i = 0; i < triangles.size(); i++){
+    vec4 v0 = triangles[i].v0;
+    vec4 v1 = triangles[i].v1;
+    vec4 v2 = triangles[i].v2;
+    vec3 e1 = vec3(v1.x-v0.x,v1.y-v0.y,v1.z-v0.z);
+    vec3 e2 = vec3(v2.x-v0.x,v2.y-v0.y,v2.z-v0.z);
+    vec3 b = vec3(start.x-v0.x,start.y-v0.y,start.z-v0.z);
+    glm::mat3 A( -b, e1, e2 );
+    vec3 x = glm::inverse( A ) * b;
+
+    if (x.y > 0 && x.z > 0 && x.x >= 0) {
+      closestIntersection.position = x.x * dir;
+      closestIntersection.distance = x.x * dir.length();
+      closestIntersection.triangleIndex = i;
+      return true;
+    }
+  }
+  return false;
 }
